9e045d84888a2f102e002c27f9118ffab5c7bb2b
waled11 <58594708+waled11@users.noreply.github.com>
2019-12-09 21:29:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1"
-var adminprefix = '1'
+var prefix = "2"
+var adminprefix = '2'
 
 
 //bc
 
 client.on("message", message => {
-    if (message.content.startsWith("1obc")) {
+    if (message.content.startsWith("2obc")) {
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
@@ -23,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "1";
+  var prefix = "2";
 
   client.on("message", message => {
   
@@ -66,23 +66,23 @@ m.sendMessage(args)
 });
 
   client.on('message', msg => {
-    if(msg.content === '1help')
+    if(msg.content === '2help')
     msg.reply('Check Your DM :white_check_mark:')
   });
   
   
   client.on("message", message => {
-    if (message.content === "1help") {
+    if (message.content === "2help") {
      const embed = new Discord.RichEmbed() 
          .setColor("#00FF00")
          .setThumbnail(message.author.avatarURL)
          .setDescription(`**Help|هيلب
 
-       1obc | لأرسال برود كاست للكل
+       2obc | لأرسال برود كاست للكل
 
-       1bc  |  لأرسال برود كاست للأونلاين
+       2bc  |  لأرسال برود كاست للأونلاين
 
-       1adminbc | برودكاست عادي
+       2adminbc | برودكاست عادي
 
        ** `)
    message.author.sendEmbed(embed)
